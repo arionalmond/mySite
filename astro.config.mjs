61054b230115820947e37b31cd8b14d832bfd5e1
Arion Almond <arionalmond@arions-mbp.lan>
2024-10-03 22:29:49 -0400

    manual instal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), compress(),],
-  site: "https://arionalmond.github.io",
-  base: "/site"
-});
+export default defineConfig({});
